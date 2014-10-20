79951091d138ed5bd964c8b09e76f350ca517c88
TaeminKim <ktm5260@gmail.com>
2014-10-20 23:24:47 +0900

    Detecting change through file's contents not length. with adding oldData(chunked)

--- io.js
+++ io.js
@@ -1,47 +1,58 @@
 var io = require('socket.io')();
 var fs = require('fs');
-var oldLength = 0;
-var nowLength = 0;
+var inspect = require('util').inspect;
+var watchingFile = './out.log';
 
 console.log('watching log.out!');
 io.on('connection', function(socket){
 	console.log('a user connected');
 	//emit first file
-	var chunkedData="";
-	fs.readFile('./out.log','UTF-8', function(err,data){
+	var oldLength=0;
+	var nowLength=0;
+	var oldData=[];
+	fs.readFile(watchingFile,'UTF-8', function(err,nowData){
+		var chunkedData=[];
 		if(!err) {
-			chunkedData=data.split('\n');
-			chunkedData.forEach(function(){
-				nowLength++;
-			});
-			nowLength--;
-			emitEntireText(chunkedData, socket, nowLength);
+			chunkedData=nowData.split('\n');
+			chunkedData.pop(); //remove blank line (마지막줄은 항상 빈 라인이 생성되는 듯 함.)
+			nowLength=chunkedData.length;
+			emitEntireText(chunkedData, socket);
 		} else {
 			console.log(err);
 		}
+		oldData=chunkedData;
 		oldLength=nowLength;
 	});
 
-
-	var watcher = fs.watch('./out.log', function(event, filename){
+	var watcher = fs.watch(watchingFile, function(event, filename){
 		console.log('file changed!');
-		fs.readFile('./out.log','UTF-8',function(err,data){
-			nowLength=0;
-			var chunkedData=[];
+		var changedType=0; //added:0  changed:1
+		fs.readFile(watchingFile,'UTF-8',function(err,nowData){
 			if(!err) {
-				chunkedData=data.split('\n');
-				chunkedData.forEach(function(){
-					nowLength++;
-				});
-				nowLength--;
-				if(nowLength<oldLength) {
-					emitEntireText(chunkedData,socket,nowLength);
-				} else if(nowLength==oldLength) {
-					//do nothing
-				} else {
-					emitAddedText(chunkedData,socket,nowLength,oldLength);
+				var chunkedData=[];
+				chunkedData=nowData.split('\n');
+				chunkedData.pop();
+				nowLength=chunkedData.length;
+				console.log("nowLength : "+nowLength+" oldLength : "+oldLength);
+				for(i=0;i<nowLength;i++) {
+					console.log("now:"+chunkedData[i]+" old:"+oldData[i]);
+					if (i>=oldLength) {
+						changedType=0;
+						break;
+					} else if(i<oldLength && chunkedData[i]!=oldData[i]) {
+						changedType=1;
+						break;
+					}
+				}
+				console.log('changedType:'+changedType);
+				switch (changedType) {
+					case 0:
+						emitAddedText(chunkedData, socket, oldLength, nowLength); break;
+					case 1:
+						emitEntireText(chunkedData, socket); break;
 				}
 				oldLength=nowLength;
+				oldData=chunkedData;
 			} else {
 				console.log(err);
 			}
@@ -51,25 +62,21 @@ io.on('connection', function(socket){
 		console.log('the user disconnected');
 		watcher.close();
 	});
-	function emitEntireText(chunkedData, socket, nowLength) {
-		var tmpLength=0;
+	function emitEntireText(chunkedData, socket) {
 		var tmpData="";
 		chunkedData.forEach(function(line){
-			tmpLength++;
-			if(nowLength>=tmpLength) tmpData=tmpData+line+"<br>";
-		})
-		socket.emit('fileChanged',tmpData);
-	}
-	function emitAddedText(chunkedData, socket, nowLength, oldLength) {
-		var tmpLength=0;
-		var tmpData="";
-		chunkedData.forEach(function(line){
-			tmpLength++;
-			if(tmpLength>oldLength && nowLength>=tmpLength) {
-				tmpData=tmpData+line+"<br>";
-			}
+			tmpData=tmpData+line+"<br>";
 		});
-		socket.emit('textAdded',tmpData);
+		console.log("entireText : "+tmpData);
+		socket.emit('changed',tmpData);
+	}
+	function emitAddedText(chunkedData, socket, oldLength, nowLength) {
+		var tmpData="";
+		for(i=oldLength;i<nowLength;i++) {
+			tmpData=tmpData+chunkedData[i]+"<br>";
+		}
+		console.log("addedText : "+tmpData);
+		socket.emit('added',tmpData);
 	}
 });
 
--- public/javascripts/addFileChanges.js
+++ public/javascripts/addFileChanges.js
@@ -1,9 +1,9 @@
 $(function(){
 	var socket = io.connect('http://localhost:3000');
-	socket.on('textAdded', function(data){
+	socket.on('added', function(data){
 		$("#filechanges").append(data);
 	});
-	socket.on('fileChanged', function(data){
+	socket.on('changed', function(data){
 		$("#filechanges").html(data);
 	})
 });
